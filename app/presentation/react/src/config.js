6ed0ce87c2d92369e9b6f8ae3438948143b30463
RayhungKao <ryamkao2339@gmail.com>
2022-08-09 22:54:20 +0800

    frontend built

--- app/presentation/react/src/config.js
+++ app/presentation/react/src/config.js
@@ -1,2 +1,3 @@
-export const baseUrl = process.env.REACT_APP_RUBY_ENV === 'develop' ? 'http://localhost:9292' :  ''
+// export const baseUrl = process.env.REACT_APP_RUBY_ENV === 'develop' ? 'http://localhost:9292' :  ''
+export const baseUrl = process.env.REACT_APP_RUBY_ENV === 'develop' ? 'http://localhost:9292' :  'https://nthu-tripbook-app.herokuapp.com'
 
